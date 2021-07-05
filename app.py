e3ec5dbb9152804c9ef58c502ec275d7aa3a2631
saurabh.saha91 <mastsaha@gmail.com>
2021-07-06 02:50:21 +0530

    adding common route, store in local_storage

--- app.py
+++ app.py
@@ -1,15 +1,36 @@
-from flask import Flask
+from flask import Flask, request, redirect
 from flask.templating import render_template, render_template_string
+
+response_mapping = {
+    'home' : 'f',
+    'f' : 'a',
+    'a' : 'v',
+    'v' : 'd',
+    'd' : 's',
+    's' : 'am',
+    'am' : 'r',
+    'r' : 'so',
+    'so' : 'ar',
+    'ar' : 'k',
+    'k' : 'thank'
+}
 
 app = Flask(__name__ , template_folder='template')
 
 @app.route("/")
 def hello():
-    return render_template('home.html')
+    return redirect('/home')
 
-@app.route("/swarm")
-def swarm():
-    return render_template('correct.html')
+@app.route("/<cclue>")
+def home(cclue):
+    return render_template(cclue+'.html')
+
+@app.route("/user_response")
+def response():
+    #print(request.args)
+    done_clue_name = request.referrer.split('/')[-1]
+    next_clue_name = response_mapping[done_clue_name]
+    return redirect('/'+next_clue_name)
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0',port='5000')
