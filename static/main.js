b6553b68490cd07a3ace4394dc756beb2d2eb6ae
saurabh.saha91 <mastsaha@gmail.com>
2021-07-15 21:24:11 +0530

    adding check submit is not empty

--- static/main.js
+++ static/main.js
@@ -13,18 +13,19 @@ function storename() {
   function storeanswer() {
     // Check browser support
     if (typeof (Storage) !== "undefined") {
-      // Store
-      // retrieve try number
+      var answer = document.getElementById('answer').value
       var cur_clue = window.location.pathname.replace('/', '')
       var value = localStorage.getItem(cur_clue)
+
+      // Store
+      // retrieve try number
       if (value == null) {
         value = { 'max': 0 }
       } else {
         value = JSON.parse(value)
         value['max'] += 1
-        value[value['max']] = document.getElementById('answer')
+        value[value['max']] = answer
       }
-
       localStorage.setItem(cur_clue, JSON.stringify(value));
     } else {
       document.getElementById("result").innerHTML = "Sorry, your browser does not support Web Storage...";
